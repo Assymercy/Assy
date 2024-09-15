3a981eb9593e9e22c14f5f3d8a5c2a2b451165bb
Assymercy <assynansamba9@gmail.com>
2024-09-15 15:34:45 +0300

    Add files via upload

---
+++ WEEK2_JAVASCRIPT/varriables.js
@@ -0,0 +1,33 @@
+//this is my first js application
+console.log("Hello world");
+
+// varriables
+//initialisation
+var my_name="ASSY NANSAMBA";
+
+//varriable declaration
+var height;
+
+//assign varriable
+height=5.4;
+
+//output name
+console.log(my_name);
+
+//output height
+console.log(height);
+
+//let
+let area=20.7;
+
+//output area
+console.log(area);
+
+//printing name and height in one line
+console.log(my_name,"-",height);
+
+//const
+const pie=8.2;
+
+//output pie
+console.log(pie);
